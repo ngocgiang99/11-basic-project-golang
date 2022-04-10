175bc5a2546c9809360212cdf97f5ccae76ab74b
truong ngoc giang <giangtn@aimesoft.com>
2022-04-10 21:52:54 +0700

    Add slack bot for upload file

---
+++ slack-file-bot/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
+
+
+func main() {
+	godotenv.Load("local.env")
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"test1.txt", "test2.txt"}
+
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File: fileArr[i],
+		}
+
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+		}	
+		fmt.Printf("Name: %s, URL: %s\n", file.Name, file.URL)
+	}
+
+}
